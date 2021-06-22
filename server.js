a6d60343bad39ece1bd7c79593240ebced203258
Ayush <aksayushx@gmail.com>
2021-06-22 23:55:56 +0530

    changed formatting

--- server.js
+++ server.js
@@ -1,68 +1,72 @@
-const express = require('express')
-const http = require('http')
-var cors = require('cors')
-const app = express()
+const express = require("express");
+const http = require("http");
+var cors = require("cors");
+const app = express();
 
-const path = require("path")
+const path = require("path");
 
-var server = http.createServer(app)
-var io = require('socket.io')(server)
+var server = http.createServer(app);
+var io = require("socket.io")(server);
 
-app.use(cors())
+app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(__dirname + "/build"))
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname + "/build/index.html"))
-	})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/build/index.html"));
+  });
 }
-app.set('port', (process.env.PORT || 4001))
+app.set("port", process.env.PORT || 4001);
 
-connections = {}
+connections = {};
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  socket.on("join-call", (path) => {
+    if (connections[path] === undefined) {
+      connections[path] = [];
+    }
+    connections[path].push(socket.id);
 
-	socket.on('join-call', (path) => {
-		if (connections[path] === undefined) {
-			connections[path] = []
-		}
-		connections[path].push(socket.id)
+    for (let a = 0; a < connections[path].length; ++a) {
+      io.to(connections[path][a]).emit(
+        "user-joined",
+        socket.id,
+        connections[path]
+      );
+    }
 
-		for (let a = 0; a < connections[path].length; ++a) {
-			io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
-		}
+    console.log(path, connections[path]);
+  });
 
-		console.log(path, connections[path])
-	})
+  socket.on("signal", (toId, message) => {
+    io.to(toId).emit("signal", socket.id, message);
+  });
 
-	socket.on('signal', (toId, message) => {
-		io.to(toId).emit('signal', socket.id, message)
-	})
+  socket.on("disconnect", () => {
+    var key;
+    for (const [k, v] of JSON.parse(
+      JSON.stringify(Object.entries(connections))
+    )) {
+      for (let a = 0; a < v.length; ++a) {
+        if (v[a] === socket.id) {
+          key = k;
 
+          for (let a = 0; a < connections[key].length; ++a) {
+            io.to(connections[key][a]).emit("user-left", socket.id);
+          }
 
-	socket.on('disconnect', () => {
-		var key
-		for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
-			for (let a = 0; a < v.length; ++a) {
-				if (v[a] === socket.id) {
-					key = k
+          var index = connections[key].indexOf(socket.id);
+          connections[key].splice(index, 1);
 
-					for (let a = 0; a < connections[key].length; ++a) {
-						io.to(connections[key][a]).emit("user-left", socket.id)
-					}
+          if (connections[key].length === 0) {
+            delete connections[key];
+          }
+        }
+      }
+    }
+  });
+});
 
-					var index = connections[key].indexOf(socket.id)
-					connections[key].splice(index, 1)
-
-					if (connections[key].length === 0) {
-						delete connections[key]
-					}
-				}
-			}
-		}
-	})
-})
-
-server.listen(app.get('port'), () => {
-	console.log("listening on", app.get('port'))
-})
+server.listen(app.get("port"), () => {
+  console.log("listening on", app.get("port"));
+});
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,63 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { v4 as uuidv4 } from 'uuid';
-
+import React from "react";
+import { Button } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 function Home() {
+  const [roomUrl, setRoomUrl] = React.useState("");
 
-	const [roomUrl, setRoomUrl] = React.useState('');
+  const joinRoom = () => {
+    if (roomUrl === "") {
+      alert("Enter Valid Room URl");
+    } else {
+      var url = roomUrl.split("/");
+      window.location.href = `/${url[url.length - 1]}`;
+    }
+  };
 
-	const joinRoom = () => {
-		if (roomUrl === '') {
-			alert("Enter Valid Room URl");
-		}
-		else {
-			var url = roomUrl.split("/");
-			window.location.href = `/${url[url.length - 1]}`;
-		}
-	}
+  const createRoom = () => {
+    var url = uuidv4();
+    window.location.href = `/${url}`;
+  };
 
-	const createRoom = () => {
-		var url = uuidv4();
-		window.location.href = `/${url}`;
-	}
-
-	return (
-		<div className="App">
-			<div className="row">
-				<div>
-					<Button onClick={() => { createRoom() }}>
-						Create a Meeting
-					</Button>
-				</div>
-				<form onSubmit={(e) => {
-					e.preventDefault();
-					joinRoom();
-				}}>
-					<div className="row">
-						<input value={roomUrl}
-							onChange={(e) =>
-								setRoomUrl(e.target.value)
-							} type="text" className="field container input-field" placeholder="Enter Meeting Link Here!">
-						</input>
-					</div>
-					<Button variant="light" className="submit-button" type="submit" value="Submit">Join Room</Button>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="row">
+        <div>
+          <Button
+            onClick={() => {
+              createRoom();
+            }}
+          >
+            Create a Meeting
+          </Button>
+        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            joinRoom();
+          }}
+        >
+          <div className="row">
+            <input
+              value={roomUrl}
+              onChange={(e) => setRoomUrl(e.target.value)}
+              type="text"
+              className="field container input-field"
+              placeholder="Enter Meeting Link Here!"
+            ></input>
+          </div>
+          <Button
+            variant="light"
+            className="submit-button"
+            type="submit"
+            value="Submit"
+          >
+            Join Room
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
