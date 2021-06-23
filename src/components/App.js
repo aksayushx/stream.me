30368bc93a6612ea11372b65bc141ada43fce2ff
Ayush <aksayushx@gmail.com>
2021-06-23 23:00:12 +0530

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
-import VideoPlayer from './VideoPlayer'
-import Home from './Home'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import VideoPlayer from "./VideoPlayer";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<Router>
-					<Switch>
-						<Route path="/" exact component={Home} />
-						<Route path="/:url" component={VideoPlayer} />
-					</Switch>
-				</Router>
-			</div>
-		)
-	}
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/:url" component={VideoPlayer} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
